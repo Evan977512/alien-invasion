4a77bad11976b6a7b802938bc2fbb1a03e9a0149
EvanKim <youngchan.kim@edu.sait.ca>
2022-07-29 22:36:51 -0600

    add background and spaceship

--- main.js
+++ main.js
@@ -11,6 +11,45 @@ document.body.appendChild(canvas);
 // images for the game
 let backgroundImage, spaceshipImage, missileImage, enemyImage, gameoverImage;
 
+// spaceship coordinate
+let spaceshipX = canvas.width / 2 - 32;
+let spaceshipY = canvas.height - 64;
+
 function loadImage() {
+  // background
   backgroundImage = new Image();
+  backgroundImage.src = "images/background.gif";
+
+  // spaceship
+  spaceshipImage = new Image();
+  spaceshipImage.src = "images/spaceship.png";
+
+  // missile
+  missileImage = new Image();
+  missileImage.src = "images/missile.png";
+
+  // enemy
+  enemyImage = new Image();
+  enemyImage.src = "images/enemy.png";
+
+  // gameover
+  gameoverImage = new Image();
+  gameoverImage.src = "images/gameover.gif";
 }
+
+// draw images on canvas
+function render() {
+  // ctx drawimages == https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
+  ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+  // draw spaceship
+  ctx.drawImage(spaceshipImage, spaceshipX, spaceshipY);
+}
+
+function main() {
+  render();
+  console.log("animation calls main function");
+  requestAnimationFrame(main);
+}
+
+loadImage();
+main();
